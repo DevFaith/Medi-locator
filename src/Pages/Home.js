923ee2eed2e9765cf533e52b84f785a49a611724
DevFaith <waitherafaith@outlook.com>
2024-09-06 01:02:55 +0300

    * feat(Home.js): add new Home component with About, Hero, HowItWorks, and Contact components

---
+++ src/Pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import About from '../../src/Components/About/About'
+import Hero from '../../src/Components/Hero/Hero'
+import HowItWorks from '../Components/HowItWorks/HowItWorks'
+import Contact from '../Components/Contacts/Contact'
+
+
+function Home() {
+    return (
+        <div>
+            <Hero />
+            <About />
+            <HowItWorks />
+            <Contact />
+        </div>
+    )
+}
+
+export default Home
